81e725a6ee8c23c0526a8c0744739a50d8fdf85b
JeshuaBenitez <jesuaeder.moshi2004@gmail.com>
2025-04-10 08:28:25 -0600

    Actualización de mycv para render

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,30 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
+describe('CVs TEst', () => {
+  it('Test header component', () => {
     cy.visit('/')
     cy.contains('Jeshua Benitez')
+  })
+  it('Test work-experience component', () => {
+    cy.visit('/')
+    cy.contains('Frontend Developer')
+  })
+  it('Test education component', () => {
+    cy.visit('/')
+    cy.contains('education works!')
+  })
+  it('Test languages component', () => {
+    cy.visit('/')
+    cy.contains('languages works!')
+  })
+  it('Test skills component', () => {
+    cy.visit('/')
+    cy.contains('skills works!')
+  })
+  it('Test certificates component', () => {
+    cy.visit('/')
+    cy.contains('certificates works!')
+  })
+  it('Test interests component', () => {
+    cy.visit('/')
+    cy.contains('interests works!')
   })
 })
